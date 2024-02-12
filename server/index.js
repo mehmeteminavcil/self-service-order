b8351539032056179b5e8a574ca9b7dae5f41496
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-02-13 02:24:06 +0300

    .env ignore and etc

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import multer from "multer"
 import userRoute from "./routes/user.js"
 import authRoute from "./routes/auth.js"
 import  productRoute from './routes/products.js'
+
 const app = express()
 
 dotenv.config()
@@ -15,8 +17,27 @@ mongoose.connect(process.env.MONGO_URL)
 })
 
 
-
+//middleware
 app.use(express.json())
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,"public/products")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+
+const upload = multer({storage});
+
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    try {
+        return res.status(200).json("file uploaded")
+    } catch (error) {n
+        console.log(error)
+    }
+})
 
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
